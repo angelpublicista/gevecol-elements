7db1cfd2a48ebb2aeac998401302a8b7108fa6e8
Angeldevelop <you@example.com>
2021-11-07 16:24:00 -0500

    first commit

---
+++ assets/js/gev-slider-1.js
@@ -0,0 +1,9 @@
+jQuery(function ($) {
+	$('.gev-slider-1').slick({
+        dots: true,
+        arrows: true,
+        nextArrow: '<div class="gev-custom-arrow gev-arrow-next"><i class="fas fa-chevron-right"></i></div>',
+        prevArrow: '<div class="gev-custom-arrow gev-arrow-prev"><i class="fas fa-chevron-left"></i></div>',
+        autplay: true
+    })
+});
