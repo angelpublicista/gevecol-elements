2eaad2bfe937ced1488dbc6656247e0738c6d567
Angeldevelop <you@example.com>
2021-11-08 15:29:31 -0500

    Slider sales finish

---
+++ assets/js/gev-slider-sales.js
@@ -0,0 +1,20 @@
+jQuery(function ($) {
+	$('.gev-slider-sales').slick({
+        slidesToShow: 3,
+        dots: true,
+        infinite: false,
+        arrows: true,
+        nextArrow: '<div class="gev-custom-arrow gev-arrow-next"><i class="fas fa-chevron-right"></i></div>',
+        prevArrow: '<div class="gev-custom-arrow gev-arrow-prev"><i class="fas fa-chevron-left"></i></div>',
+        autoplay: true,
+        speed: 1500,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+});
